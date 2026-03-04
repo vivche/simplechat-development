07348b5fddda1038a4cd81eaeb0c331e91dc0005
Chen, Vivien <Vivien.Chen+ecolab@ecolab.com>
2026-03-03 19:00:38 -0500

    Move MI model fetch fix doc to v0.238.025 and bump version

--- application/single_app/config.py
+++ application/single_app/config.py
@@ -88,7 +88,7 @@ load_dotenv()
 EXECUTOR_TYPE = 'thread'
 EXECUTOR_MAX_WORKERS = 30
 SESSION_TYPE = 'filesystem'
-VERSION = "0.238.024"
+VERSION = "0.238.025"
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key-change-in-production')
 
